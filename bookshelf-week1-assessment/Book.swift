8418b639e20731e87457e7463901d39c139026c4
cdmccune <curtmccune@gmail.com>
2022-05-20 08:54:49 -0700

    "setup completed"

---
+++ bookshelf-week1-assessment/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  bookshelf-week1-assessment
+//
+//  Created by Curt McCune on 5/20/22.
+//
+
+import Foundation
---
+++ bookshelf-week1-assessment/BookController.swift
@@ -0,0 +1,8 @@
+//
+//  BookController.swift
+//  bookshelf-week1-assessment
+//
+//  Created by Curt McCune on 5/20/22.
+//
+
+import Foundation
